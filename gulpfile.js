b06a89071f2922355370fbfe2bf2b5854ac776a9
Christophe GERARD <christophe.gerard8@gmail.com>
2016-09-01 21:35:54 +0200

    BIM

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,11 @@ var vulcanize = require('gulp-vulcanize');
 
 gulp.task('copy', function() {
     return gulp.src(['front/index.html','front/bower_components/webcomponentsjs/webcomponent-lite.js'])
+            .pipe(vulcanize());
            .pipe(gulp.dest('dist'));
 });
 gulp.task('vulcanize',function() {
-    return gulp.src([	'front/src/my-app.html','front/src/my-diploma.html',
-                     	'front/src/my-experiences.html',
-                     	'front/src/my-history.html',
-                     	'front/src/my-icons.html',
-                     	'front/src/my-projects.html',
-                     	'front/src/my-skills',
-                     	'front/src/my-svg'])
+    return gulp.src(['front/src/my-app.html'])
         .pipe(vulcanize({
         stripeComments : true,
         inlineScripts : true,

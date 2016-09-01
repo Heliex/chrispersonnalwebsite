aba3d6fe9c09f5321d55c94a3d01d6a520ada6f9
Christophe GERARD <christophe.gerard8@gmail.com>
2016-09-01 21:02:32 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var vulcanize = require('gulp-vulcanize');
 
 gulp.task('copy', function() {
     return gulp.src(['front/index.html','front/bower_components/webcomponentsjs/webcomponent-lite.js'])
-           .pipe(gulp.dest('front/dist'));
+           .pipe(gulp.dest('dist'));
 });
 gulp.task('vulcanize',function() {
     return gulp.src([	'front/src/my-app.html','front/src/my-diploma.html',
@@ -18,7 +18,7 @@ gulp.task('vulcanize',function() {
         inlineScripts : true,
         inlineCss : true
     }))
-        .pipe(gulp.dest('front/dist'));
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default',['vulcanize']);

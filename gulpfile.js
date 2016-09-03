052a55944a1cb6450a0ae1dac24f0ab8c4a34eb7
Christophe GERARD <christophe.gerard8@gmail.com>
2016-09-03 10:22:34 +0200

    testgulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('copy', function() {
             .pipe(gulp.dest('dist'));
 });
 gulp.task('vulcanize',function() {
-    return gulp.src(['front/src/elements.html'])
+    return gulp.src(['front/src/elements.html','front/src/my-diploma.html'])
         .pipe(vulcanize({
         stripeComments : true,
         inlineScripts : true,

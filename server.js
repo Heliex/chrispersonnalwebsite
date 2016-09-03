cec350939904ec2f0459392c927b294ca169a519
Christophe GERARD <christophe.gerard8@gmail.com>
2016-09-03 10:26:47 +0200

    test sur le serveur

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'front')));
 // ---------- Définition des routes -------------
 
+app.get('src/my-history.html',function(req,res){
+    
+    console.log("test");
+});
 app.listen(80);
 
 

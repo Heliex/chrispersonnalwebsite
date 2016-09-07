2f19f8ee038f72c36d875fa89b446c8990dc81f5
Christophe GERARD <christophe.gerard8@gmail.com>
2016-09-07 02:08:16 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,24 @@ var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
 
 gulp.task('copy', function() {
-    return gulp.src(['front/index.html','front/bower_components/webcomponentsjs/webcomponent-lite.js'])
+    return gulp.src('front/index.html')
             .pipe(vulcanize({
+            abspath : '',
 			stripeComments : true,
 			inlineScripts : true,
-			inlineCss : true}))
+			inlineCss : true,
+            excludes: ['//fonts.googleapis.com/*']
+    }))
             .pipe(gulp.dest('dist'));
 });
 gulp.task('vulcanize',function() {
     return gulp.src(['front/src/elements.html'])
         .pipe(vulcanize({
+        abspath : '',
         stripeComments : true,
         inlineScripts : true,
-        inlineCss : true
+        inlineCss : true,
+        excludes : ['//fonts.googleapis.com/*']
     }))
         .pipe(gulp.dest('dist/src'));
 });
